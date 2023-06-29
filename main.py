dd83dc81afa9c16291921f55779ef4cbb1e98717
Cavid2002 <quliyevcavid132@gmail.com>
2023-06-29 20:56:19 +0400

    authentication stage

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,Cookie
+from fastapi.responses import RedirectResponse, HTMLResponse
 from werkzeug.security import generate_password_hash, check_password_hash
 from models import User,session
 from auth import *
@@ -6,19 +7,34 @@ from auth import *
 app = FastAPI()
 
 
-data = [
-	{"name" : "David", "surname": "Anderson", "age" : 22},
-	{"name" : "Simon", "surname": "Henriks", "age" : 21},
-	{"name" : "Samantha", "surname": "Shane", "age" : 21}
-]
+@app.post("/sign-up")
+async def signUp(new_user: UserCreateBase):
+    if(session.query(User).filter(User.username == new_user.username).first()):
+        return { "msg" : "Username already taken"}
+    if(session.query(User).filter(User.email == new_user.email).first()):
+        return { "msg" : "Email already in use"}
+    
+    user = User(username=new_user.username,
+                password=generate_password_hash(new_user.password),
+                age = new_user.age,
+                gender=new_user.gender)
+    user.save()
 
 
-@app.route("/", methods=['GET', 'POST'])
-async def login(user_data: UserSignInBase):
+
+
+@app.post("/")
+async def login(user_data: UserSignInBase, cook: Cookie()):
 	user = session.query(User).filter(User.mail == user_data.email).first()
 	if(user == None):
 		return {"msg" : "User doesn't exists"}
 	
 	if(check_password_hash(user.password, user_data.password)):
 		encodeJWT(user.id)
-		
+		return {"msg" : "User logged in"}
+
+
+@app.get("/main")
+async def main():
+    pass
+			
